1f2a59853eb34bdaa7eca36afbdfeb3f33b0a0c7
aldomaz <aldomazrocket@gmail.com>
2023-03-23 17:48:29 -0500

    Feat: Cloudinary for images

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,19 +1,14 @@
 module.exports = [
   'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "*.digitaloceanspaces.com"
-          ],
-          "media-src": ["'self'", "data:", "blob:"],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,19 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-dos",
-        providerOptions: {
-          key: env('DO_SPACE_ACCESS_KEY'),
-          secret: env('DO_SPACE_SECRET_KEY'),
-          endpoint: env('DO_SPACE_ENDPOINT'),
-          space: env('DO_SPACE_BUCKET'),
-          directory: env('DO_SPACE_DIRECTORY')
-        },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
+  },
 
     email: {
       config: {

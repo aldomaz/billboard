1c6866b8063a4f009a6f4f98319177f5f2c57b4c
aldomaz <aldomazrocket@gmail.com>
2023-03-23 16:30:40 -0500

    Fix: upload dos

--- config/plugins.js
+++ config/plugins.js
@@ -3,12 +3,11 @@ module.exports = ({ env }) => ({
       config: {
         provider: "strapi-provider-upload-dos",
         providerOptions: {
-          key: process.env.DO_SPACE_ACCESS_KEY,
-          secret: process.env.DO_SPACE_SECRET_KEY,
-          endpoint: process.env.DO_SPACE_ENDPOINT,
-          space: process.env.DO_SPACE_BUCKET,
-          directory: process.env.DO_SPACE_DIRECTORY,
-          cdn: process.env.DO_SPACE_CDN,
+          key: env('DO_SPACE_ACCESS_KEY'),
+          secret: env('DO_SPACE_SECRET_KEY'),
+          endpoint: env('DO_SPACE_ENDPOINT'),
+          space: env('DO_SPACE_BUCKET'),
+          directory: env('DO_SPACE_DIRECTORY')
         },
       },
     },
